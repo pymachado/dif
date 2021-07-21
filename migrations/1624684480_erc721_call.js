3f56b9ba93d82b050fef2687e1c87acdb2c44b0c
pymachado <pyuris22@gmail.com>
2021-07-20 21:26:04 -0400

    after_deploy_TokenFactoring_and_PooLRBAC

--- migrations/1624684480_erc721_call.js
+++ migrations/1624684480_erc721_call.js
@@ -1,6 +1,20 @@
 const ERC721 = artifacts.require("TokenFactoring");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(ERC721);
-
+const PoolRBAC = artifacts.require("PooLRBAC");
+const erc20Address = "0x1593145c85fD0041649e51d7d67d2a205E5e5e5D";
+module.exports = async function(deployer, network, accounts) {
+  const [admin, moderator, funder, _] = accounts;
+  if (network === 'ropsten' || network === 'development') {
+    await deployer.deploy(ERC721, {from: admin});
+    const tokenFactoring = await ERC721.deployed();
+    await deployer.deploy(PoolRBAC, 
+      erc20Address, 
+      tokenFactoring.address, 
+      moderator, 
+      funder, 
+      web3.utils.toWei("2000", "ether"), 
+      web3.utils.toWei("10", "ether"),
+      360, 
+      web3.utils.toWei("500", "ether"), {from: admin});
+  }
+  
 };
--- truffle-config.js
+++ truffle-config.js
@@ -70,9 +70,10 @@ module.exports = {
        }),
      network_id: 3,       // Ropsten's id
      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
      networkCheckTimeout: 1000000,
+     confirmations: 2,    // # of confs to wait between deployments. (default: 0)     
      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+     //deploymentPollingInterval: 8000,
      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      },
     // Useful for private networks
